91ee8f8cb2006267c63edd548e6203a55314e505
EnzoFelyx <enzofelyx@gmail.com>
2024-12-10 16:57:11 -0300

    api integration

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,18 +1,17 @@
 import { View } from 'react-native'
+import { router } from 'expo-router'
 
 import { Button } from '@/components/button'
 import { Steps } from '@/components/steps'
 import { Welcome } from '@/components/welcome'
 import { styles } from './styles'
 
-
-
 export default function Index() {
     return (
         <View style={styles.container}>
             <Welcome />
             <Steps />
-            <Button >
+            <Button onPress={() => router.navigate("/home")}>
                 <Button.Title>Começar</Button.Title>
             </Button>
         </View>
